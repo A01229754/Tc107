283f9628f51cf32e8d5aa60879a7bec1c0fd4906
A01229754 <a01229754@gmail.com>
2015-02-05 20:56:40 -0600

    Programa para poder hacer operaciones de 2 numeros

---
+++ wsq03.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Dame un primer numero a introducir: ";
+	cin>>a;
+	cout<<"Dame un segundo numero a introducir: ";
+	cin>>b;
+	cout<<"La suma de los 2 numeros es: "<<a+b<<endl;
+	cout<<"La resta de los 2 numeros es: "<<a-b<<endl;
+	cout<<"La division de los 2 numeros es: "<<a/b<<endl;
+	cout<<"El residuo de la division entre los 2 numeros es: "<<a%b<<endl;
+
+	return 0;
+}
