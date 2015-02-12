aa25becaed21c0b0fc6a1a924d7b69692b8fd308
A01229754 <a01229754@gmail.com>
2015-02-12 01:13:55 -0600

    Divertidi juego de logica con numeros

--- justfun.cpp
+++ justfun.cpp
@@ -8,8 +8,36 @@ using namespace std;
 int main()
 {
 	srand(time(NULL));
-	int z,j,x;
+	int z,j,x,i=1;
 	z=rand()%101;
-	cout<<"Tu numero es "<<z<<endl;
-	return z;	
+	cout<<"Estoy pensando en un numero del 1 al 100, ¿Cual es?"<<endl;
+	cin>>j;
+
+	while(j!=z)
+	{
+		i++;
+		if(j<z)
+		{
+			cout<<"Lo siento pero "<<j<<" es menor al numero, Intentalo de nuevo!"<<endl;
+			cin>>j;
+		}else
+		{	
+			cout<<"Lo siento pero "<<j<<" es mayor al numero, Intentalo de nuevo!"<<endl;
+			cin>>j;
+		}
+	}
+	while(j==z)
+	{
+		cout<<"Muy bien porfin...el numero de intentos toales fue "<<i<<endl;
+		if(i<5)
+		{
+			cout<<"Wow!!! Eres asombroso"<<endl;
+			return 0;
+		}else
+		{
+			cout<<"Necesitas mejorar tu habilidad de logica!!"<<endl;
+			return 0;
+		}
+	}
+return 0;
 }
