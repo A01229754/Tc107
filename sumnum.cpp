39588a70bca2263f89f5445c2a1c18d2c12e1f7d
A01229754 <a01229754@gmail.com>
2015-02-12 22:02:24 -0600

    Suma de los numeros hasta llegar al valor mas alto

---
+++ sumnum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	int l,u,x=1;
+	cout<<"Dame el numero mas bajo a sumar"<<endl;
+	cin>>l;
+	cout<<"Dame el numero mas alto a sumar"<<endl;
+	cin>>u;
+
+	while(l!=u)
+	{
+		l++;
+		x=x+y;
+	}
+	
+	cout<<"El resultado de la suma del "<<l<<" al "<<u<<" es de: "<<x<<endl;
+	return 0;
+}
