e9e43fcdf07f3a3c298137c35a5138a973f2f0f1
A01229754 <a01229754@gmail.com>
2015-02-16 09:34:52 -0600

    Code for mathematics operation in C++ with comments

--- wsq03.cpp
+++ wsq03.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
-#include<math.h>
-using namespace std;
-int main()
+#include<math.h> //this is the new include for the librery of math for C
+using namespace std;//this is standar for every program
+int main()//the main code
 {
 	int a,b;
-	cout<<"Dame un primer numero a introducir: ";
+	cout<<"Dame un primer numero a introducir: ";//dos valores dados por el ussuario
 	cin>>a;
 	cout<<"Dame un segundo numero a introducir: ";
 	cin>>b;
-	cout<<"La suma de los 2 numeros es: "<<a+b<<endl;
-	cout<<"La resta de los 2 numeros es: "<<a-b<<endl;
-	cout<<"La division de los 2 numeros es: "<<a/b<<endl;
-	cout<<"El residuo de la division entre los 2 numeros es: "<<a%b<<endl;
+	cout<<"La suma de los 2 numeros es: "<<a+b<<endl;//suma
+	cout<<"La resta de los 2 numeros es: "<<a-b<<endl;//resta
+	cout<<"La division de los 2 numeros es: "<<a/b<<endl;//division entera
+	cout<<"El residuo de la division entre los 2 numeros es: "<<a%b<<endl;//residuo entero
 
-	return 0;
+	return 0;//para que ya no te regrese ningun valor
 }
